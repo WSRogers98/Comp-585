aacc1c8ec53167d8b51f6fc4d3712e2223dbc557
ruthwang97 <46601888+ruthwang97@users.noreply.github.com>
2020-04-22 18:28:27 -0400

    with sign in and half of the lobby

--- lib/GameScreenEnd.dart
+++ lib/GameScreenEnd.dart
@@ -5,7 +5,17 @@ import 'package:flutter/widgets.dart';
 import 'dart:math' as math;
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
-
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math' as math;
+import 'package:Cherokee/lobby.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:Cherokee/lobbyO.dart';
+import 'package:Cherokee/GameScreenW.dart';
+import 'package:Cherokee/main.dart';
+import 'package:google_fonts/google_fonts.dart';
 // TODO: get timer to automatically start
 //done
 void main() => runApp(GameEnd());
@@ -47,12 +57,47 @@ class _MyHomePageState extends State<GamePage> with TickerProviderStateMixin {
 
   }
 
+  Widget buildS() {
+    return Column(children: <Widget>[
+      Text("Final Score"),
+      StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance
+              .collection('gameSessions')
+              .document(joinedRoom)
+              .collection('players')
+              .snapshots(),
+          builder: (context, snapshot) {
+            var userName;
+            var votes;
+            var score;
+            String scoreBoard = '';
+            var length = snapshot.data.documents.length;
+            for (int i = 0; i < length; i++) {
+              userName = snapshot.data.documents[i].documentID;
+              votes = snapshot.data.documents[i].data['vote'].toString();
+              score = snapshot.data.documents[i].data['score'].toString();
+              scoreBoard = scoreBoard +
+                  "\n" +
+                  userName +
+                  "\n" +
+                  "current round: " +
+                  votes +
+                  "\n" +
+                  "total score: " +
+                  score;
+            }
+            return Text(scoreBoard);
+          }),
+
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
 
     const thiscolor = const Color(0x6BA7B5);
-    return Text("end");
+    return buildS();
   }
 }
 
